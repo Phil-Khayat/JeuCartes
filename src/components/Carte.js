af87a4dc17993036570aab512941391192f623a8
Philippe Khayat <pkhayat@hotmail.com>
2022-04-06 19:30:00 +0200

    third-send-06-04-2022

--- src/components/Carte.js
+++ src/components/Carte.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import '../css/Carte.css';
 
-function Carte ({carte}) {
+// function Carte ({carte, img}) {
+class Carte extends Component {
 
-    let angle = Math.random() * 90 - 45;
-    let xPos = Math.random() * 40 - 20;
-    let yPos = Math.random() * 40 - 20;
-    let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+    constructor(props) {
+        super(props);
+        let angle = Math.random() * 90 - 45;
+        let xPos = Math.random() * 40 - 20;
+        let yPos = Math.random() * 40 - 20;
+        this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+    }
+
+    // let angle = Math.random() * 90 - 45;
+    // let xPos = Math.random() * 40 - 20;
+    // let yPos = Math.random() * 40 - 20;
+    // let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
 
     // console.log("transform syntax : ", transform);
 
+    render () {
     return (
         <div>
             {/* <img src={carte.image} style={transform}></img> */}
-            <img className="Carte" src={carte.image} style={ {transform} }></img>
+            {/* <img className="Carte" src={carte.image} style={ {transform} }></img> */}
+            {/* <img className="Carte" src={img} style={ {transform} }></img> */}
+            <img className="Carte" src={this.props.img} style={ {transform: this._transform} }></img>
         </div>
     );
+    }
 }
 
 export default Carte;
--- src/components/JeuCartes.js
+++ src/components/JeuCartes.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Carte from './Carte';
+import '../css/JeuCartes.css';
 
 const API_CARTES=`https://deckofcardsapi.com/api/deck/`
 const API_JEU_CARTES=`${API_CARTES}new/shuffle/`;
@@ -26,15 +27,18 @@ class JeuCartes extends Component {
         // console.log(API_NEW_CAR_URL);
         let responseCard = await axios.get(API_NEW_CAR_URL);
         // console.log(responseCard);
-        this.setState( { cartes: [...this.state.cartes, responseCard.data.cards[0]] });
+
+        // this.setState( { cartes: [...this.state.cartes, responseCard.data.cards[0]] });
+        this.setState( (prevState) => ( { cartes: [...prevState.cartes, responseCard.data.cards[0]] }));
         // console.log(this.state.cartes);
     }
 
     render() {
-        let cartes = this.state.cartes.map( (c) => <Carte key={c.code} carte={c}/>)
+        // let cartes = this.state.cartes.map( (c) => <Carte key={c.code} carte={c}/>)
+        let cartes = this.state.cartes.map( (c) => <Carte key={c.code} img={c.image}/>)
         return (
             <div>
-                <button onClick={this.retrieveCarte}>Récupérer une carte</button>
+                <button className="buttonClass" onClick={this.retrieveCarte}>Récupérer une carte</button>
                 <div>{cartes}</div>
             </div>
         );
